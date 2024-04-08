428e8eee45ce2e399ef0fe1428348d29ae5a87bc
Lucas-1048 <lucas.almeida1048@gmail.com>
2024-04-07 21:50:57 -0300

    Feature: adição de cors no backend

--- Backend/src/server/server.ts
+++ Backend/src/server/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { router } from "./routes/route";
+import cors from "cors";
 
 const server = express();
 
-server
-    .use(express.json())
-    .use(router)
+server.use(cors());
+
+server.use(express.json());
+server.use(router);
 
 export { server };
